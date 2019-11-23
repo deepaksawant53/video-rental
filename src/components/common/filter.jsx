9e446ed613178c0be7ada52433ec4cb9cf94d1ae
Deepak Sawant <deepak.sawant53@gmail.com>
2019-11-24 00:53:39 +0530

    Refactoring of code done.

--- src/components/common/filter.jsx
+++ src/components/common/filter.jsx
@@ -2,19 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Filter = props => {
-  const { elementList, selectedElement, onClick, textProperty, valueProperty } = props;
-
+  const { elementList, selectedElement, onElementSelected, textProperty, valueProperty } = props;
   return (
     <ul className="list-group">
-      {elementList.map(element => <li style={{ cursor: "pointer" }} key={element[valueProperty]} onClick={() => onClick(element)} className={element[valueProperty] === selectedElement ? "list-group-item active" : "list-group-item"}>{element[textProperty]}</li>)}
+      {elementList.map(element => <li style={{ cursor: "pointer" }} key={element[valueProperty]} onClick={() => onElementSelected(element)} className={element[valueProperty] === selectedElement._id ? "list-group-item active" : "list-group-item"}>{element[textProperty]}</li>)}
     </ul>
   );
 };
 
 Filter.propTypes = {
   elementList: PropTypes.array.isRequired,
-  selectedElement: PropTypes.number.isRequired,
-  onClick: PropTypes.func.isRequired
+  selectedElement: PropTypes.object.isRequired,
+  onElementSelected: PropTypes.func.isRequired
 }
 
 Filter.defaultProps = {
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -12,11 +12,12 @@ class movies extends Component {
     pageSize: 4,
     currentPage: 1,
     genres: [],
-    selectedElement: 0
+    selectedGenre: {}
   }
 
   componentDidMount() {
-    this.setState({ movies: getMovies(), genres: [{ _id: 0, name: 'All Genres' }, ...getGenres()] });
+    const defaultGenre = { _id: 0, name: 'All Genres' }
+    this.setState({ movies: getMovies(), genres: [{ ...defaultGenre }, ...getGenres()], selectedGenre: defaultGenre });
   }
 
   handleDeleteMovie = movieId => {
@@ -38,18 +39,18 @@ class movies extends Component {
 
   handleFilter = genre => {
     const filteredMovies = genre.name === 'All Genres' ? getMovies() : getMovies().filter(movie => movie.genre._id === genre._id);
-    this.setState({ selectedElement: genre._id, movies: filteredMovies, currentPage: 1 });
+    this.setState({ selectedGenre: genre, movies: filteredMovies, currentPage: 1 });
   }
 
   render() {
     const { length: count } = this.state.movies;
-    const { pageSize, currentPage, movies: allMovies, genres, selectedElement } = this.state;
+    const { pageSize, currentPage, movies: allMovies, genres, selectedGenre } = this.state;
     if (count === 0) return <p>There are no movies in the database</p>
     const movies = paginate(allMovies, currentPage, pageSize);
     return (
       <div className="row">
         <div className="col-md-3">
-          <Filter elementList={genres} selectedElement={selectedElement} onClick={this.handleFilter} />
+          <Filter elementList={genres} selectedElement={selectedGenre} onElementSelected={this.handleFilter} />
         </div>
         <div className="col">
           <span>Showing {count} movies in the database</span>
