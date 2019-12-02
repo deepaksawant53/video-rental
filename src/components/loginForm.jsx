db7cfe69badad7c2477c6f8e9f3b98a59bd78a34
Deepak Sawant <deepak.sawant53@gmail.com>
2019-12-02 23:21:12 +0530

    Changes done to capture input in password field in a variable inside state.

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -13,24 +13,25 @@ class LoginForm extends Component {
     console.log('Username', username);
   }
 
-  handleChange = e => {
+  handleChange = ({ currentTarget: input }) => {
     const account = { ...this.state.account };
-    account.username = e.currentTarget.value;
+    account[input.name] = input.value;
     this.setState({ account });
   }
 
   render() {
+    const { account } = this.state;
     return (
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
-            <input onChange={this.handleChange} value={this.state.account.username} autoFocus ref={this.username} id="username" type="text" className="form-control" />
+            <input name="username" onChange={this.handleChange} value={account.username} autoFocus ref={this.username} id="username" type="text" className="form-control" />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
+            <input name="password" onChange={this.handleChange} value={account.password} id="password" type="text" className="form-control" />
           </div>
           <button className="btn btn-primary">Login</button>
         </form>
