db812e9e08385a5c697f0895f55753c5ce904c05
Deepak Sawant <deepak.sawant53@gmail.com>
2019-12-03 23:46:35 +0530

    Changes done to extract out input fields into Input component.

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
+import Input from './common/input';
 
 class LoginForm extends Component {
   state = {
-    account: { username: '', password: '' }
+    account: { username: '', password: '' },
   }
-  username = React.createRef();
+
   handleSubmit = e => {
     e.preventDefault();//This prevents the default submit action in which the whole page gets reloaded and due to which bundle.js and login.html was getting reloaded.
 
     // Here we can write logic to Call the server
-    const username = this.username.current.value;
-    console.log('Username', username);
+    console.log('Submitted')
   }
 
   handleChange = ({ currentTarget: input }) => {
@@ -25,14 +25,17 @@ class LoginForm extends Component {
       <div>
         <h1>Login</h1>
         <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <input name="username" onChange={this.handleChange} value={account.username} autoFocus ref={this.username} id="username" type="text" className="form-control" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input name="password" onChange={this.handleChange} value={account.password} id="password" type="text" className="form-control" />
-          </div>
+          <Input
+            name="username"
+            value={account.username}
+            label="Username"
+            onChange={this.handleChange} />
+          <Input
+            name="password"
+            value={account.password}
+            label="Password"
+            onChange={this.handleChange} />
+
           <button className="btn btn-primary">Login</button>
         </form>
       </div>
