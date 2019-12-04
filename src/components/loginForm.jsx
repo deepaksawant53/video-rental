6249488d1f1aa2db2d86ddf71f668c0bb80df19f
Deepak Sawant <deepak.sawant53@gmail.com>
2019-12-04 23:53:36 +0530

    Changes done to validate individual fields using joi-browser validation library.

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -6,16 +6,19 @@ class LoginForm extends Component {
   state = {
     account: { username: '', password: '' },
     errors: {}
-  }
+  };
 
   // This we are writing outside state because this will be initialised only once. 
   // It is a schema for joi validation library.
   schema = {
     username: Joi.string().required().label('Username'),// Label is used to define friendly name of field in the error message.
     password: Joi.string().required().label('Password')
-  }
+  };
 
   validate = () => {
+    // This abortEarly property is of Joi and by default it is true.
+    // What happens if it is true is that whenever it will get first field with error it will exit from their itself and it wont check for other fields.
+    // But in this method we want to validate our entire page hence we dont want abortEarly behaviour here hence we are explicitly setting it as false.
     const options = { abortEarly: false };
     // Error object returned by Joi.validate() is as follows
     // {"error":
@@ -36,7 +39,7 @@ class LoginForm extends Component {
     const errors = {};
     for (let item of error.details) errors[item.path[0]] = item.message;
     return errors;
-  }
+  };
 
   handleSubmit = e => {
     e.preventDefault();//This prevents the default submit action in which the whole page gets reloaded and due to which bundle.js and login.html was getting reloaded.
@@ -48,16 +51,16 @@ class LoginForm extends Component {
 
     // Here we can write logic to Call the server
     console.log('Submitted')
-  }
+  };
 
   validateProperty = ({ name, value }) => {
-    if (name === "username") {
-      if (value === "") return "Username is required!!!"
-    }
-    if (name === "password") {
-      if (value === "") return "Password is required!!!"
-    }
-  }
+    const obj = { [name]: value };
+    const schema = { [name]: this.schema[name] };
+    // Here we are not setting abortEarly to false because we want to validate single fields
+    // and hence when it finds any field mentioned in schema to violate the rule it should give us the error message.
+    const { error } = Joi.validate(obj, schema);
+    return error ? error.details[0].message : null;
+  };
 
   handleChange = ({ currentTarget: input }) => {
     const errors = { ...this.state.errors };
@@ -68,7 +71,7 @@ class LoginForm extends Component {
     const account = { ...this.state.account };
     account[input.name] = input.value;
     this.setState({ account, errors });
-  }
+  };
 
   render() {
     const { account, errors } = this.state;
@@ -95,6 +98,6 @@ class LoginForm extends Component {
         </form>
       </div>
     );
-  }
+  };
 }
 export default LoginForm;
