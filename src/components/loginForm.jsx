5970c2d622efa9c779b688648b3a85245e97a20c
Deepak Sawant <deepak.sawant53@gmail.com>
2019-12-05 23:30:32 +0530

    Changes done to disable login button if there is some error on form.

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -94,7 +94,7 @@ class LoginForm extends Component {
             error={errors.password}
           />
 
-          <button className="btn btn-primary">Login</button>
+          <button disabled={this.validate()} className="btn btn-primary">Login</button>
         </form>
       </div>
     );
