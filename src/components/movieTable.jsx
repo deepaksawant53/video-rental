c515ba4b2eef6c09576f10b169d56015f0bcfc47
Deepak Sawant <deepak.sawant53@gmail.com>
2020-01-12 23:53:24 +0530

    Changes done to add authentication and authorization on Delete button.

--- src/components/movieTable.jsx
+++ src/components/movieTable.jsx
@@ -1,24 +1,41 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Like from './common/like';
 import Table from './common/table';
-import { Link } from 'react-router-dom';
+import authService from '../services/authService';
 
 class MoviesTable extends Component {
+  state = {
+    columns: [
+      { key: 'title', propertyName: 'title', label: 'Title', jsxContent: movie => <Link to={`/movie/${movie._id}`}>{movie.title}</Link> },
+      { key: 'genre', propertyName: 'genre.name', label: 'Genre' },
+      { key: 'numberInStock', propertyName: 'numberInStock', label: 'Stock' },
+      { key: 'dailyRentalRate', propertyName: 'dailyRentalRate', label: 'Rate' },
+      { key: 'like', jsxContent: movie => <Like liked={movie.liked} onClick={() => this.props.onLike(movie)} /> },
+      { key: 'delete', jsxContent: movie => <button onClick={() => this.props.onDelete(movie._id)} className="btn btn-danger btn-sm">Delete</button> }
+    ]
+  };
+
+  componentDidMount() {
+    const user = authService.getCurrentUser();
+    const columns = [
+      { key: 'title', propertyName: 'title', label: 'Title', jsxContent: movie => <Link to={`/movie/${movie._id}`}>{movie.title}</Link> },
+      { key: 'genre', propertyName: 'genre.name', label: 'Genre' },
+      { key: 'numberInStock', propertyName: 'numberInStock', label: 'Stock' },
+      { key: 'dailyRentalRate', propertyName: 'dailyRentalRate', label: 'Rate' },
+      { key: 'like', jsxContent: movie => <Like liked={movie.liked} onClick={() => this.props.onLike(movie)} /> },
+      { key: 'delete', jsxContent: user && user.isAdmin ? movie => <button onClick={() => this.props.onDelete(movie._id)} className="btn btn-danger btn-sm">Delete</button> : "" }
+    ]
+    this.setState({columns});
+  };
+
   // We are initializing this column property here because throughout the life of this component
   // the value of this property is never going to be changed.
-  columns = [
-    { key: 'title', propertyName: 'title', label: 'Title', jsxContent: movie => <Link to={`/movie/${movie._id}`}>{movie.title}</Link> },
-    { key: 'genre', propertyName: 'genre.name', label: 'Genre' },
-    { key: 'numberInStock', propertyName: 'numberInStock', label: 'Stock' },
-    { key: 'dailyRentalRate', propertyName: 'dailyRentalRate', label: 'Rate' },
-    { key: 'like', jsxContent: movie => <Like liked={movie.liked} onClick={() => this.props.onLike(movie)} /> },
-    { key: 'delete', jsxContent: movie => <button onClick={() => this.props.onDelete(movie._id)} className="btn btn-danger btn-sm">Delete</button> }
-  ];
 
   render() {
     const { movies, sortColumn, onSort } = this.props;
     return (
-      <Table data={movies} columns={this.columns} sortColumn={sortColumn} keyPropertyForBody={{ name: '_id' }} onSort={onSort} />
+      <Table data={movies} columns={this.state.columns} sortColumn={sortColumn} keyPropertyForBody={{ name: '_id' }} onSort={onSort} />
     );
   }
 }
