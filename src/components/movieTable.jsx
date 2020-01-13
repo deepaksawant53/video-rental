df3f9d3373e1d772a5d1c2f5432d17225c9e841a
Deepak Sawant <deepak.sawant53@gmail.com>
2020-01-13 23:50:02 +0530

    Done some refactoring for hiding/showing delete column based on user role.

--- src/components/movieTable.jsx
+++ src/components/movieTable.jsx
@@ -5,29 +5,21 @@ import Table from './common/table';
 import authService from '../services/authService';
 
 class MoviesTable extends Component {
-  state = {
-    columns: [
-      { key: 'title', propertyName: 'title', label: 'Title', jsxContent: movie => <Link to={`/movie/${movie._id}`}>{movie.title}</Link> },
-      { key: 'genre', propertyName: 'genre.name', label: 'Genre' },
-      { key: 'numberInStock', propertyName: 'numberInStock', label: 'Stock' },
-      { key: 'dailyRentalRate', propertyName: 'dailyRentalRate', label: 'Rate' },
-      { key: 'like', jsxContent: movie => <Like liked={movie.liked} onClick={() => this.props.onLike(movie)} /> },
-      { key: 'delete', jsxContent: movie => <button onClick={() => this.props.onDelete(movie._id)} className="btn btn-danger btn-sm">Delete</button> }
-    ]
-  };
+  columns = [
+    { key: 'title', propertyName: 'title', label: 'Title', jsxContent: movie => <Link to={`/movie/${movie._id}`}>{movie.title}</Link> },
+    { key: 'genre', propertyName: 'genre.name', label: 'Genre' },
+    { key: 'numberInStock', propertyName: 'numberInStock', label: 'Stock' },
+    { key: 'dailyRentalRate', propertyName: 'dailyRentalRate', label: 'Rate' },
+    { key: 'like', jsxContent: movie => <Like liked={movie.liked} onClick={() => this.props.onLike(movie)} /> },
+  ];
 
-  componentDidMount() {
+  deleteColumn = { key: 'delete', jsxContent: movie => <button onClick={() => this.props.onDelete(movie._id)} className="btn btn-danger btn-sm">Delete</button> };
+
+  constructor() {
+    super();
     const user = authService.getCurrentUser();
-    const columns = [
-      { key: 'title', propertyName: 'title', label: 'Title', jsxContent: movie => <Link to={`/movie/${movie._id}`}>{movie.title}</Link> },
-      { key: 'genre', propertyName: 'genre.name', label: 'Genre' },
-      { key: 'numberInStock', propertyName: 'numberInStock', label: 'Stock' },
-      { key: 'dailyRentalRate', propertyName: 'dailyRentalRate', label: 'Rate' },
-      { key: 'like', jsxContent: movie => <Like liked={movie.liked} onClick={() => this.props.onLike(movie)} /> },
-      { key: 'delete', jsxContent: user && user.isAdmin ? movie => <button onClick={() => this.props.onDelete(movie._id)} className="btn btn-danger btn-sm">Delete</button> : "" }
-    ]
-    this.setState({columns});
-  };
+    if (user && user.isAdmin) this.columns.push(this.deleteColumn);
+  }
 
   // We are initializing this column property here because throughout the life of this component
   // the value of this property is never going to be changed.
@@ -35,7 +27,7 @@ class MoviesTable extends Component {
   render() {
     const { movies, sortColumn, onSort } = this.props;
     return (
-      <Table data={movies} columns={this.state.columns} sortColumn={sortColumn} keyPropertyForBody={{ name: '_id' }} onSort={onSort} />
+      <Table data={movies} columns={this.columns} sortColumn={sortColumn} keyPropertyForBody={{ name: '_id' }} onSort={onSort} />
     );
   }
 }
